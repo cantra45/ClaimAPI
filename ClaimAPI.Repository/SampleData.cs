b744e2aa4432850bdc5bb91408e36e947b9d9946
cantra45 <cantra45@gmail.com>
2021-03-17 16:56:22 +0530

    Add project files.

---
+++ ClaimAPI.Repository/SampleData.cs
@@ -0,0 +1,50 @@
+﻿using ClaimAPI.Repository.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClaimAPI.Repository
+{
+    public class SampleData
+    {
+        public List<Member> GetMembers()
+        {
+            List<Member> members = new List<Member>();
+            //Adding sample member data
+            members.Add(new Member()
+            {
+                MemberID = 1123,
+                FirstName = "John",
+                LastName = "Doe",
+                EnrollmentDate = Convert.ToDateTime("9/1/2020")
+            });
+            members.Add(new Member()
+            {
+                MemberID = 1245,
+                FirstName = "Jane",
+                LastName = "Doe",
+                EnrollmentDate = Convert.ToDateTime("10/3/2020")
+            });
+            return members;
+        }
+
+        public List<Claim> GetClaims()
+        {
+            List<Claim> claims = new List<Claim>();
+            //Adding sample Claim data
+            claims.Add(new Claim()
+            {
+                MemberID = 1123,
+                ClaimAmount = 1112.56M,
+                ClaimDate = Convert.ToDateTime("10/6/2020")
+            });
+            claims.Add(new Claim()
+            {
+                MemberID = 1245,
+                ClaimAmount = 67.54M,
+                ClaimDate = Convert.ToDateTime("12/5/2020")
+            });
+            return claims;
+        }
+    }
+}
---
+++ ClaimAPI/Controllers/ClaimController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ClaimAPI.Repository;
+using ClaimAPI.Repository.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClaimController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetClaimsByDate(DateTime startDate, DateTime endDate)
+        {
+            // Declare Model
+            List<MemberClaimModel> memberClaimModels = new List<MemberClaimModel>();
+            //Getting Claims and Members list from repository
+            List<Claim> claims = new SampleData().GetClaims();
+            List<Member> members = new SampleData().GetMembers();
+            // Getting filtered claims depend on provided start date and end date(between dates)
+            List<Claim> filterClaims = (from row in claims
+                                        where row.ClaimDate < endDate && row.ClaimDate > startDate
+                                        select row).ToList();
+            if (filterClaims.Count > 0)
+            {
+                var distinctMemberIds = filterClaims.Select(p => p.MemberID).Distinct().ToList();
+                if (distinctMemberIds.Count > 0)
+                {
+
+                    for (var i = 0; i < distinctMemberIds.Count; i++)
+                    {
+                        // Adding multiple members with cliams if multiple members exists in filterClaims Object
+                        memberClaimModels.Add(new MemberClaimModel()
+                        {
+                            Member = members.Where(p => p.MemberID == distinctMemberIds[i]).FirstOrDefault(),
+                            ClaimDetails = filterClaims.Where(p => p.MemberID == distinctMemberIds[i]).ToList()
+                        });
+                    }
+                }
+            }
+            return Ok(memberClaimModels);
+        }
+    }
+}
